546789a2dd0a1fabab378b893e6253c225f0925d
Brian Park <park-brian@users.noreply.github.com>
2023-05-25 12:15:53 -0400

    update outDir

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,8 @@ export default defineConfig({
   server: {
     port: 4000,
   },
+  build: {
+    outDir: "../docs",
+  },
   plugins: [react()],
-})
+});

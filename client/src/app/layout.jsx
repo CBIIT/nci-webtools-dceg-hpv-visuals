93f8ead83797635ae1f43107ef650b70580b9225
Thuong Nguyen <6723699+nhoktenz@users.noreply.github.com>
2023-10-19 10:51:58 -0400

    add goverment shutdown banner

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -6,15 +6,19 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import "./styles/main.scss";
 
-const inter = Inter({ subsets: ["latin"], variable: "--bs-font-sans-serif", display: "block" });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--bs-font-sans-serif",
+  display: "block",
+});
 
 export default function RootLayout({ children }) {
   const routes = [
-    { title: "Home", path: "/"},
+    { title: "Home", path: "/" },
     { title: "View Images", path: "/view" },
     { title: "About", path: "/about" },
     { title: "Publications", path: "/publications" },
-  ]
+  ];
 
   return (
     <RecoilRoot>
@@ -24,10 +28,16 @@ export default function RootLayout({ children }) {
           <meta name="keywords" content="HPVAVE" />
           <link rel="icon" href="/favicon.ico" sizes="any" />
           <GoogleAnalytics id={process.env.GOOGLE_ANALYTICS_ID} />
+          <script src="https://cbiit.github.io/nci-softwaresolutions-elements/components/include-html.js"></script>
         </head>
-        <body className={[inter.className, "d-flex flex-column vh-100"].join(" ")}>
+        <body
+          className={[inter.className, "d-flex flex-column vh-100"].join(" ")}
+        >
+          <include-html src="https://cbiit.github.io/nci-softwaresolutions-elements/banners/government-shutdown.html"></include-html>
           <Header routes={routes} />
-          <main className="position-relative d-flex flex-column flex-grow-1 align-items-stretch bg-light">{children}</main>
+          <main className="position-relative d-flex flex-column flex-grow-1 align-items-stretch bg-light">
+            {children}
+          </main>
           <Footer />
         </body>
       </html>
